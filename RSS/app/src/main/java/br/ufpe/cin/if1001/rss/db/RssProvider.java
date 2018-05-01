c677118a352a377659793a75df75067b1be1e4a2
luisdelgado <lhds@cin.ufpe.br>
2018-05-01 15:40:43 -0300

    Implementando RssProvider

--- RSS/app/src/main/java/br/ufpe/cin/if1001/rss/db/RssProvider.java
+++ RSS/app/src/main/java/br/ufpe/cin/if1001/rss/db/RssProvider.java
@@ -7,11 +7,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
-import static android.provider.BaseColumns._ID;
+import static br.ufpe.cin.if1001.rss.db.SQLiteRSSHelper.DATABASE_TABLE;
+import static br.ufpe.cin.if1001.rss.db.SQLiteRSSHelper.ITEM_LINK;
 
 public class RssProvider extends ContentProvider {
 
     private SQLiteRSSHelper db;
+    Context c;
 
     public RssProvider() {
     }
@@ -19,27 +21,41 @@ public class RssProvider extends ContentProvider {
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Implement this to handle requests to delete one or more rows.
-        //SQLiteDatabase db =  c.getWritableDatabase();
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = this.db.getWritableDatabase();
+        db.delete(DATABASE_TABLE, selection, selectionArgs);
+        return db.delete(DATABASE_TABLE, selection, selectionArgs);
     }
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
+
         // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String type = "";
+        SQLiteDatabase db = this.db.getWritableDatabase();
+        String search = "SELECT * FROM " + DATABASE_TABLE + " WHERE " +
+                ITEM_LINK + " = ?";
+        Cursor cursor = db.rawQuery(search, new String[] {String.valueOf(uri)});
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                type = cursor.getString(5);
+            }
+            cursor.close();
+        }
+        return type;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = this.db.getWritableDatabase();
+        if (db.insert(DATABASE_TABLE, null, values) == -1) uri = null;
+        return uri;
     }
 
     @Override
     public boolean onCreate() {
+        c = this.getContext();
         if (db == null) {
-            //
+            db = new SQLiteRSSHelper(this.getContext());
         }
         return false;
     }
@@ -47,14 +63,14 @@ public class RssProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = this.db.getWritableDatabase();
+        return db.query(DATABASE_TABLE, projection, selection, selectionArgs, null, null, sortOrder);
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = this.db.getWritableDatabase();
+        return db.update(DATABASE_TABLE, values, selection, selectionArgs);
     }
 }
